42877eff0706b6824a421e2d98b5f5a7f216f776
nkhaldi <mnk80898@gmail.com>
2023-06-29 02:00:37 +0400

    migrated to DB

--- books/admin.py
+++ books/admin.py
@@ -5,15 +5,16 @@ from books.models import Author, Book
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = "__all__"
-    fields = "__all__"
-    search_fields = "__all__"
+    list_display = ("name",)
+    fields = ("name",)
+    search_fields = ("name",)
     ordering = ("name",)
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = "__all__"
-    fields = "__all__"
-    search_fields = "__all__"
-    ordering = ("title", "author", "series", "genre")
+
+    list_display = ("title", "series", "author", "genre",)
+    fields = ("title", "series", "author", "genre",)
+    search_fields = ("title", "series", "author", "genre",)
+    ordering = ("title", "series", "author", "genre",)
