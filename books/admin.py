dcbf0fea546b28866af0e096dac112a20d43f3c2
nkhaldi <mnk80898@gmail.com>
2023-06-29 01:41:46 +0400

    registered Book and Author in admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = "__all__"
+    fields = "__all__"
+    search_fields = "__all__"
+    ordering = ("name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = "__all__"
+    fields = "__all__"
+    search_fields = "__all__"
+    ordering = ("title", "author", "series", "genre")
