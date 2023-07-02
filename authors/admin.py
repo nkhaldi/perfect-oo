832708c7fca5232097457bc0acde3b537ba7c17d
nkhaldi <mnk80898@gmail.com>
2023-07-02 19:14:30 +0400

    added authors admin panel

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from authors.models import Author
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    fields = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
